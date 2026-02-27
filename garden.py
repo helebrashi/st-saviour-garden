b4784b6e6fe3fcc3bb74ffb2ee95c7f98403d7c3
Hana <helebrashi28@stsaviour.org>
2026-02-27 08:39:43 -0500

    stardew ally

--- garden.py
+++ garden.py
@@ -3,4 +3,63 @@
 if __name__ == '__main__':
 
     # print without a line break using this function
-    print('🌷', end='')
+    # print('🌷', end='') 
+
+    for row in range(11):
+        for col in range(11):
+            print("🌼", end=" ")
+        print('')
+print("\n" + "="*30 + "\n")
+
+for row in range(11):
+    for col in range(11):
+        if row % 2 == 0:
+            print("🌷", end=" ")
+        else:
+            print("🌼", end=" ")
+    print()
+print("\n" + "="*30 + "\n")
+
+for row in range(11):
+    for col in range(11):
+        if col % 2 == 0:
+            print("🌷", end=" ")
+        else:
+            print("🌼", end=" ")
+    print()
+print("\n" + "="*30 + "\n")
+
+for row in range(11):
+    for col in range(11):
+        if row == col:
+            print("🌷", end=" ")
+        else:
+            print("🌼", end=" ")
+    print()
+print("\n" + "="*30 + "\n")
+
+for row in range(11):
+    for col in range(11):
+        if row % 2 == 0 and col % 2 == 0:
+            print("🌷", end=" ")
+        else:
+            print("🌼", end=" ")
+    print()
+print("\n" + "="*30 + "\n")
+
+for row in range(11):
+    for col in range(11):
+        if row % 2 == 0 and col % 2 == 0:
+            print("🌷", end=" ")
+        else:
+            print("🌼", end=" ")
+    print()
+print("\n" + "="*30 + "\n")
+
+for row in range(11):
+    for col in range(11):
+        if row == col or row + col == 10:
+            print("🌷", end=" ")
+        else:
+            print("🌼", end=" ")
+    print()
